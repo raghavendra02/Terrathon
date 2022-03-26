744716c18f3970543679c84f5e9c65a9b512feff
raghavendra02 <akraghavendra02@gmail.com>
2022-03-26 14:38:09 +0530

    created a file  for receiveing money donantions

--- Frontend/src/components/Donation.js
+++ Frontend/src/components/Donation.js
@@ -1,21 +1,23 @@
 import React from 'react'
+import { ListGroup } from 'react-bootstrap'
 import Money from './donation/Money'
 import Food from './donation/Money'
 
 
 const DonatePage = () => {
-    return(
-        <div>
-        <div className='Slide-show-donate'>
-        </div>
-        <div className='Choice-food-money'>
-            <h3 className='Title-donate'>How do you wish to contribute?</h3>
-            <h4 className='Options'>Donate Money</h4>
-            <h4 className='Options'>Donate Food</h4>
-        </div>
-
+    return (
+        <div className='body-donate'>
+            <div className='Slide-show-donate'>
+            </div>
+            <div className='Choice-food-money'>
+                <h3 className='Title-donate'>How do you wish to contribute?</h3>
+                <ListGroup>
+                    <ListGroup.Item onClick={drirectToMoney}>Donate Money</ListGroup.Item>
+                    <ListGroup.Item onClick={drirectToFood}>Donate Food</ListGroup.Item>
+                </ListGroup>
+            </div>
         </div>
     )
 }
- 
+
 export default DonatePage
